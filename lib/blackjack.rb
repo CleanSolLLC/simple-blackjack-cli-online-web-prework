75f3b37a694f4c490532d31543f602fd4ff21b55
Mark May <mamark90@gmail.com>
2019-03-16 12:10:10 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -24,9 +24,15 @@ def end_game(card_total)
 end
 
 def initial_round
+<<<<<<< HEAD
   first_deal = deal_card + deal_card
   display_card_total(first_deal)
   return first_deal
+=======
+  card_total = deal_card + deal_card
+  display_card_total(card_total)
+  return card_total
+>>>>>>> fd846d02dd40f5c004773d63c8078173eb1d859b
 end
 
 
@@ -34,6 +40,7 @@ def hit?(card_total)
   prompt_user
   user_input = get_user_input
 
+<<<<<<< HEAD
 
   if user_input == "h"
   card_total += deal_card
@@ -41,14 +48,25 @@ def hit?(card_total)
     elsif user_input == "s"
       card_total
 
+=======
+    if user_input == "s"
+      card_total
+
+    elsif user_input == "h"
+      card_total += deal_card
+
+>>>>>>> fd846d02dd40f5c004773d63c8078173eb1d859b
     else
       invalid_command
       prompt_user
       get_user_input
 
     end
+<<<<<<< HEAD
 #return card_total
 
+=======
+>>>>>>> fd846d02dd40f5c004773d63c8078173eb1d859b
 end
 
 def invalid_command
@@ -64,7 +82,11 @@ def runner
   card_total = initial_round
 
     until card_total > 21
+<<<<<<< HEAD
       card_total = hit?(card_total)
+=======
+      card_total = hit?
+>>>>>>> fd846d02dd40f5c004773d63c8078173eb1d859b
       display_card_total(card_total)
     end
       end_game(card_total)
